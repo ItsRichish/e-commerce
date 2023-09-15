6ff92130edca33f04f726f72b478b1ff5959cda3
Richish <richishm19@gmail.com>
2023-09-15 15:05:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import {
+  Home,
+  Category,
+  SingleProduct,
+  Navbar,
+  Footer,
+  Newsletter,
+} from "./components";
+import AppContext from "./utils/context";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppContext>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/category/:id" element={<Category />} />
+          <Route exact path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </AppContext>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
